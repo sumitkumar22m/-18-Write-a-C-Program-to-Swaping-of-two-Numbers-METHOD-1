ad27bbf8bdd5709d1c38d8f27a9591e623805063
sumitkumar22m <118610704+sumitkumar22m@users.noreply.github.com>
2022-11-25 21:29:45 +0530

    Add files via upload

---
+++ METHOD-2/Write a C Program to Swaping of two Numbers METHOD-2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=10,b=20;
+    printf("Before Swaping A=%d\n",a);
+    printf("Before Swaping B=%d\n\n",b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("After Swaping A=%d\n",a);
+    printf("After Swaping B=%d\n",b);
+    getch();
+}
